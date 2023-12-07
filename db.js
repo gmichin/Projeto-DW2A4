f17bb3c755f11d501c195d6a8ff2bcdb32a18b36
Gustavo <gmassamichi@gmail.com>
2023-12-06 21:37:48 -0300

    save

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'gustavo',
-  password: 'juliemei2014',
-  database: 'your_music',
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'gustavo',
+  password: process.env.DB_PASSWORD || 'juliemei2014',
+  database: process.env.DB_DATABASE || 'your_music',
 });
+
 
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err);
-    return;
+    process.exit(1);
   }
   console.log('Conectado ao banco de dados MySQL');
 });
+
 
 module.exports = db;
